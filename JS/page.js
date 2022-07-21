f8368c26e479c3c640425260b1f1c74624b230a5
Osvaldo Ortiz <workheber620@gmail.com>
2022-07-20 21:30:31 -0500

    Primera entrega

---
+++ JS/page.js
@@ -0,0 +1,9 @@
+// Declaro la palabra que se va a repetir
+let hola = "Hola";
+// Pido el número de veces que quieran que se repita esa palabra
+let num = prompt("Ingresa el número de veces que quieras que te saluden")
+
+// Sugiero que se muestre Hola siguiendo la iteración anterior
+for (i = 0; i < num; i++){
+    console.log(hola)
+}
